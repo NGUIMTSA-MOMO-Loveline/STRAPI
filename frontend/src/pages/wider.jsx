d1be885085e3dd41ad9ef603caede9f10707f391
chalomKossivi <sd.cosmos.1812@gmail.com>
2025-05-06 12:15:54 +0200

    Acceuil debut

---
+++ frontend/src/pages/wider.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Wider = () => {
+
+
+    return (
+        <>
+            <Navbar />
+        </>
+    )
+}
+
+export default Wider;
